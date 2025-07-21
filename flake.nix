e44d6e12e5860c2b4623567d9d4b2a4eef2e803b
UrbsKali <urbain.lantres@tutanota.com>
2025-07-21 18:40:50 +0200

    fix normaly

--- flake.nix
+++ flake.nix
@@ -56,29 +56,11 @@
             } ];
             
             # Import hardware configuration from .
-            imports = [ ./hardware-configuration.nix ];
+            imports = [ /etc/nixos/hardware-configuration.nix ];
           }
           
           # Apply overlays
           { nixpkgs.overlays = overlays; }
-          {
-            # Systemd tmpfiles configuration for auto-mounting drives
-            systemd.tmpfiles.rules = [
-              "d /media/hdd1 0755 root root -"
-            ];
-
-            # Auto-mount sdb drive to /media/hdd1
-            fileSystems."/media/hdd1" = {
-              device = "/dev/disk/by-uuid/${hddUUID}";
-              fsType = "ext4";
-              options = [ 
-                "defaults"
-                "nofail"             # Don't fail boot if the drive is not present
-                "x-systemd.automount" # Enable systemd automount
-                "x-systemd.device-timeout=10" # Timeout after 10 seconds if device not found
-              ];
-            };
-          }
         ] ++
         # Only include k3s modules if isMaster and masterIP are defined
         (if isMaster != null && masterIP != null then [
