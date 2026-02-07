6d17f5aa1d95f75b0dd5717229d2343d96b13b4c
UrbsKali <urbain.lantres@tutanota.com>
2026-02-07 17:07:03 +0100

    fix path

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -32,7 +32,7 @@
   };
 
   # Sops Configuration
-  sops.defaultSopsFile = ../../../secrets/secrets.yaml;
+  sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/var/lib/sops-nix/key.txt"; # Ensure this key exists on the host
 
