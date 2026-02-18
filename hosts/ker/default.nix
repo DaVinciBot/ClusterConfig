8adbcb226f13c01f5704574071b7f49422f84bc8
Come404 <come.wozniak@edu.devinci.fr>
2026-02-18 14:43:41 +0100

    Added ssd

--- hosts/ker/default.nix
+++ hosts/ker/default.nix
@@ -17,4 +17,10 @@
 
   networking.defaultGateway = "192.168.1.1";
 
+  fileSystems."/media/ssd1" = lib.mkForce {
+    device = "/dev/disk/by-uuid/41bc848a-7ff6-45c7-8fe1-e49d4a76028a";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" "x-systemd.automount" "x-systemd.device-timeout=10" ];
+  };
+
 }
