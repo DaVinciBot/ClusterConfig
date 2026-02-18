8c02206410b881f10448ec6ed70f260637f1a334
UrbsKali <urbain.lantres@tutanota.com>
2026-02-18 13:22:37 +0100

    edit ker networking

--- hosts/ker/default.nix
+++ hosts/ker/default.nix
@@ -9,8 +9,10 @@
   services.clusterTunnel.secretName = "tunnel_ker";
 
   networking.hostName = "ker";
-  networking.interfaces.eno1.ipv4.addresses = [ {
-    address = "192.168.0.10";
+  networking.interfaces.eno2.ipv4.addresses = [ {
+    address = "192.168.1.10";
     prefixLength = 24;
   } ];
+  networking.defaultGateway = "192.168.1.1";
+
 }
--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -3,7 +3,7 @@
 {
   # Networking
   networking.networkmanager.enable = true;
-  networking.defaultGateway = "192.168.0.1";
+  networking.defaultGateway = lib.mkDefault "192.168.0.1";
   networking.nameservers = ["8.8.8.8" "8.8.4.4"];
 
 }
